52a8b7bfef042a96a173f3954bbabc10bf630ba4
Luis Silva <seplusi_arcanjo@hotmail.com>
2020-11-08 17:49:00 +0100

    added another versionj

--- test1.py
+++ test1.py
@@ -7,21 +7,24 @@ from ddt import ddt, data
 
 @ddt
 class TestGitHubApi(unittest.TestCase):
-    """A sample test class to show how page object works"""
 
     headers = {'Content-Type': 'application/json', "Accept": "application/json"}
     url = "https://api.github.com/"
+    repo_2_exist = "one_repo"
+    repo_2_be_created = "repo_2_be_created"
+    repo_2_be_deleted = "repo_2_be_deleted"
+    repo_to_be_updated = "ohpen_exercise"
 
     @classmethod
     def setUpClass(cls):
-        if "Hello-World" in cls._get_all_repos():
-            cls._delete_one_repos("Hello-World")
+        if cls.repo_2_be_created in cls._get_all_repos():
+            cls._delete_one_repos(cls.repo_2_be_created)
 
-        if "repo_2_be_deleted" not in cls._get_all_repos():
-            cls._create_repo("repo_2_be_deleted")
+        if cls.repo_2_be_deleted not in cls._get_all_repos():
+            cls._create_repo(cls.repo_2_be_deleted)
 
-        if "one_repo" not in cls._get_all_repos():
-            cls._create_repo("one_repo")
+        if cls.repo_2_exist not in cls._get_all_repos():
+            cls._create_repo(cls.repo_2_exist)
 
     def setUp(self):
         pass
@@ -31,24 +34,26 @@ class TestGitHubApi(unittest.TestCase):
         response = requests.get(url=url, headers=self.headers, auth=('seplusi', 'Luis!23$'))
         assert response.status_code == 200
         assert len(json.loads(response.text)) > 0
-        assert "one_repo" in [repo['name'] for repo in  json.loads(response.text)]
+        assert self.repo_2_exist in [repo['name'] for repo in  json.loads(response.text)]
 
     def test_get_repo_properties(self):
-        url = "%srepos/seplusi/ohpen_exercise" % self.url
+        url = "%srepos/seplusi/%s" % (self.url, self.repo_to_be_updated)
         response = requests.get(url=url, headers=self.headers, auth=('seplusi', 'Luis!23$'))
         assert response.status_code == 200, 'Expected STATUS_CODE=200. Got STATUS_CODE:%d' %response.status_code
-        assert json.loads(response.text)['name'] == "ohpen_exercise", 'Expected repo name = ohpen_exercise. Got: %s' \
-                                                                      %json.loads(response.text)['name']
-        assert json.loads(response.text)['full_name'] == "seplusi/ohpen_exercise", 'Expected repo full name = seplusi/ohpen_exercise. Got: %s' %json.loads(response.text)['full_name']
+        assert json.loads(response.text)['name'] == self.repo_to_be_updated, 'Expected repo name = %s. Got: %s' \
+                                                                      %(self.repo_to_be_updated,
+                                                                        json.loads(response.text)['name'])
+        assert json.loads(response.text)['full_name'] == "seplusi/%s" %self.repo_to_be_updated, \
+            'Expected repo full name = seplusi/%s. Got: %s' %(self.repo_to_be_updated, json.loads(response.text)['full_name'])
 
     @data(1, 2)
     def test_update_repository11(self, value):
-        url = "%srepos/seplusi/ohpen_exercise" % self.url
+        url = "%srepos/seplusi/%s" % (self.url, self.repo_to_be_updated)
         data = {'description': 'test_%s' %value}
         response = requests.patch(url=url, headers=self.headers, data=json.dumps(data), auth=('seplusi', 'Luis!23$'))
         assert response.status_code == 200
-        assert json.loads(response.text)['name'] == "ohpen_exercise"
-        assert json.loads(response.text)['full_name'] == "seplusi/ohpen_exercise"
+        assert json.loads(response.text)['name'] == self.repo_to_be_updated
+        assert json.loads(response.text)['full_name'] == "seplusi/%s" % self.repo_to_be_updated
         assert json.loads(response.text)['description'] == "test_%s" %value
 
         for _ in range(10):
@@ -60,13 +65,13 @@ class TestGitHubApi(unittest.TestCase):
         else:
             assert False, 'Timeout. Update repo\'s description = \'test_%s\' did not happer' %value
 
-        assert json.loads(response.text)['name'] == "ohpen_exercise"
-        assert json.loads(response.text)['full_name'] == "seplusi/ohpen_exercise"
+        assert json.loads(response.text)['name'] == self.repo_to_be_updated
+        assert json.loads(response.text)['full_name'] == "seplusi/%s" % self.repo_to_be_updated
         assert json.loads(response.text)['description'] == "test_%s" % value
 
     def test_create_repo(self):
         new_repo_data = {
-            "name": "Hello-World",
+            "name": self.repo_2_be_created,
             "description": "This is your first repository",
             "homepage": "https://github.com",
             "private": False,
@@ -79,25 +84,25 @@ class TestGitHubApi(unittest.TestCase):
         assert response.status_code == 201
 
         for _ in range(10):
-            if "Hello-World" in self._get_all_repos():
+            if self.repo_2_be_created in self._get_all_repos():
                 break
             time.sleep(0.5)
         else:
-            assert False, 'Timeout. Hello-world repo was never created. Current list of repos is: %s'\
-                          %str(self._get_all_repos())
+            assert False, 'Timeout. %s repo was never created. Current list of repos is: %s'\
+                          %(self.repo_2_be_created, str(self._get_all_repos()))
 
     def test_delete_repo(self):
-        url = '%srepos/seplusi/Hello-World' %self.url
+        url = '%srepos/seplusi/%s' %(self.url, self.repo_2_be_deleted)
         response = requests.delete(url=url, headers=self.headers, auth=('seplusi', 'Luis!23$'))
         assert response.status_code == 204
 
         for _ in range(10):
-            if "Hello-World" not in self._get_all_repos():
+            if self.repo_2_be_deleted not in self._get_all_repos():
                 break
             time.sleep(0.5)
         else:
-            assert False, 'Timeout. Hello-world repo was not deleted. Current list of repos is: %s'\
-                          %str(self._get_all_repos())
+            assert False, 'Timeout. %s repo was not deleted. Current list of repos is: %s'\
+                          %(self.repo_2_be_deleted, str(self._get_all_repos()))
 
     @classmethod
     def _create_repo(self, repo_name):
